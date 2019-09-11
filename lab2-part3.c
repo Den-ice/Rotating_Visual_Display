a6849d271e29030a2a3ffe4bebb25460a47ef85b
Sean8499 <50029716+Sean8499@users.noreply.github.com>
2019-09-10 19:08:29 -0700

    fix/verify glitch
    how did this work?  PORTB_OUT += (PORTB_OUT + 0x01) && 0x0F ;

--- lab2-part3.c
+++ lab2-part3.c
@@ -1,8 +1,8 @@
-*
+/*
  * lab2-pt3.c
  *
  * Created: 9/10/2019 11:21:07 AM
- * Author : Sean
+ * Authors : Sean Gow & Denice Hickethier 
  */ 
 
 #include <avr/io.h>
@@ -15,8 +15,8 @@ ISR(      RTC_OVF_vect       ){
 
 if(count%3==0)
 		PORTB_OUT-= 0x10;
-		
-		PORTB_OUT += (PORTB_OUT +0x01) && 0x0F ;
+	                     //modify only low nibble    //preserve high nibble         
+		PORTB_OUT = ((PORTB_OUT +0x01) && 0x0F) + (PORTB_OUT && 0xf0) ;
 		count++;
 }
 
@@ -28,16 +28,10 @@ int main(void)
 
 cli();                           //clear interrupt counter & register
 
-	CLK_RTCCTRL = CLK_RTCSRC_RCOSC_gc | CLK_RTCEN_bm;
-	RTC_PER=500;
-	RTC_INTCTRL=PMIC_LOLVLEX_bm;
-	RTC_CTRL = 0x01;
-	RTC_INTCTRL = PMIC_LOLVLEX_bm;
-	
-	/*TCC0_PER=0x3D1;					//set period counter to above isr
-	TCC0_INTCTRLA= PMIC_MEDLVLEN_bm; //int med
-	TCC0_CTRLA=0x07;					//use 1X divider
-	*/
+	CLK_RTCCTRL = CLK_RTCSRC_RCOSC_gc | CLK_RTCEN_bm;//rtc clock setup
+	RTC_PER=500; //set 500 ticks
+	RTC_INTCTRL=PMIC_LOLVLEX_bm;//set low priority
+	RTC_CTRL = 0x01; //set granularity to 1ms
 	PMIC_CTRL = PMIC_MEDLVLEN_bm|PMIC_LOLVLEX_bm;   // turn on medium priority interrupts & LOW PRIORITY INTERRUPT
 	sei();
 while(1){
