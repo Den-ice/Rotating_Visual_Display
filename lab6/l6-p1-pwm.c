12f831541f088014e8db63ed68ce009948b1776e
Sean8499 <50029716+Sean8499@users.noreply.github.com>
2019-10-19 16:06:38 -0700

    fixed timer calc, cleaned up pwm

--- lab6/l6-p1-pwm.c
+++ lab6/l6-p1-pwm.c
@@ -1,5 +1,6 @@
 /*
  * Lab6-pt1 pwm
+ * see pictures for granularity  ~30-31us
  * Created: 10/16/19 3:16:32 PM
  * Author : Sean Gow and Denice Hickethier
  */
@@ -19,17 +20,30 @@ void setup_compare_timer();
 void setup_peripherals();
 void setup_gpio(); 
 /*end of fun */
-void tcc0_pwm_control(uint8_t ,unsigned long , uint8_t );
-
+void tcc0_pwm_control(uint8_t ,uint16_t , uint8_t );
+volatile int input_changed=0;
+volatile int duty=8;
+unsigned long calc_period(uint8_t time, uint16_t t_scale);
+unsigned long pClk=32000000;
 
 ISR (TCC0_OVF_vect){	
-	
+	if (TCC0_CCA!=0)
+	{
+	PORTC_OUT|=0X01;			
+	PORTR_OUT^=0x02;
+	}
 
     }
 	
 ISR(TCC0_CCA_vect){  
+	if (TCC0_CCA==0){
 	PORTC_OUT|=0X01;			
 	PORTR_OUT^=0x02;
+	}
+	else{
+	PORTC_OUT=PORTC_OUT&~0X01;			
+	PORTR_OUT|=0X02;
+	}
 }
 		
 ISR(TCC0_CCB_vect){ 
@@ -47,8 +61,15 @@ ISR( PORTF_INT0_vect ){
 		duty=duty+10;
 	else if (input_changed&0x04)
 		duty=duty-10;
-	tcc0_pwm_phase_correct(10,(unsigned long)pow(10,6),duty);
 
+	if (TCC0_CCA==0)//PHASE CORRECT PWM
+		TCC0_CCB=TCC0_PER*duty/100;
+	else //REGULAR PWM
+		TCC0_CCA=TCC0_PER*duty/100;
+
+//	tcc0_pwm_phase_correct(10,1000,duty);
+	
+	
 		}
 
 // call used peripheral setups
@@ -67,10 +88,10 @@ int main(void)
 	//PORTC.PIN0CTRL=PORT_OPC_PULLUP_gc;
 	PORTR_DIR=0X2;
 	setup_peripherals();
-	//10 ,ms , 8%
-	//tcc0_pwm_control(10,pow(10,6),8);
 	
-	tcc0_pwm_phase_correct(10,(unsigned long)pow(10,6),8);
+	//10 ,ms , 8% duty cycle
+	tcc0_pwm_control(10,pow(10,3),8);
+	//tcc0_pwm_phase_correct(10,1000,8);
 while (1)
     {
     }
@@ -116,13 +137,13 @@ void setup_compare_timer(){
 }
 
 
-void tcc0_pwm_control(uint8_t time, unsigned long t_scale, uint8_t duty_cycle){
-	int i=0;
-	int prescale=1024;
-	uint16_t per=0;
-	unsigned long pClk=32000000;
-	/* 1s period =pclk/divisor */
+
+unsigned long calc_period(uint8_t time, uint16_t t_scale){
+	uint8_t i;
+	uint16_t prescale=1024;
+	unsigned long per=0;
 	
+	/* 1s period =pclk/divisor */
 	/*
 	2^10=1024  0x7
 	2^8=256   0x6
@@ -133,53 +154,29 @@ void tcc0_pwm_control(uint8_t time, unsigned long t_scale, uint8_t duty_cycle){
 	2^0=1    0x1   
 	*/
 	//turn bitvalue into value for calculation
-	for (i=7;i>TCC0_CTRLA;i--){
+	for (i=7;i>(TCC0_CTRLA & 0x7);i--){  
 		prescale=prescale/2;
 		if (i>4) //prescale decrement by factor of 4 if >4, or 2 <=4
-		prescale=prescale/2;
+			prescale=prescale/2;
+		else if (i==4) 
+			prescale=prescale/4; 
     }
-	t_scale=t_scale/prescale;
-	per=pClk/prescale;
-	per= per /t_scale*time;
-//	per=per*duty_cycle;
-	// 1SECOND/TSCALE*(TIME)
-	TCC0_PER=per+4;
-	TCC0_CCA=(TCC0_PER*duty_cycle)/100+1;	
+	per=pClk/prescale*time/t_scale;
+	
+	return per;	
+}
+
+void tcc0_pwm_control(uint8_t time, uint16_t t_scale, uint8_t duty_cycle){
+	TCC0_PER=calc_period(time,t_scale)+4;
+	TCC0_CCA=(TCC0_PER*duty_cycle)/100;	
 }
 
 
-void tcc0_pwm_phase_correct(uint8_t time, unsigned long t_scale, uint8_t duty_cycle){
-	int i=0;
-	int prescale=1024;
-	uint16_t per;
-	unsigned long pClk=32000000;
-
-	/* 1s period =pclk/divisor */
+void tcc0_pwm_phase_correct(uint8_t time, uint16_t t_scale, uint8_t duty_cycle){
 	
-	/*
-	2^10=1024  0x7
-	2^8=256   0x6
-	2^6=64   0x5
-	2^3=8    0x4
-	2^2=4    0x3
-	2^1=2    0x2
-	2^0=1    0x1   
-	*/
-	//turn bitvalue into value for easier calculation
-	for (i=7;i>TCC0_CTRLA;i--){
-		prescale=prescale/2;
-		if (i>4) //prescale decrement by factor of 4 if >4, or 2 <=4
-		prescale=prescale/2;
-    }
-	t_scale=t_scale/prescale;
-	per=pClk/prescale;
-	per= per /t_scale*time;
-//	per=per*duty_cycle;
-	// 1SECOND/TSCALE*(TIME)
-	TCC0_PER=per+4;
+	TCC0_PER=calc_period(time,t_scale)+4;
 	TCC0_CCA=0;//(TCC0_PER*duty_cycle/2)/100;
-	TCC0_CCB=(TCC0_PER*duty_cycle)/100+1;	
-	
+	TCC0_CCB=(TCC0_PER*duty_cycle)/100;	
 	TCC0_CTRLB=1<<TC_WGMODE_DSTOP_gc; //ENABLE DUAL SLOPE PWM,  OTHER OPTIONS: DS_T DS_B DSBOTH
 }
 
