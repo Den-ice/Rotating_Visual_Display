b38bbba775f4f8f29154ebe04265d6e4cc643c11
Sean <Sean@Ryzen>
2019-10-25 00:37:20 -0700

    test mplab ide github sync

---
+++ lab7/lab7.X/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: Sean
+ *
+ * Created on October 25, 2019, 12:34 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
