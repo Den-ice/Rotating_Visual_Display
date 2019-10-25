2f94174e714c18549ea7a3b7be53e0704b76130b
Sean <Sean@Ryzen>
2019-10-25 00:42:35 -0700

    test mplab ide github sync

---
+++ lab7/lab7.X/build/default/production/newmain.o.d
@@ -0,0 +1,22 @@
+build/default/production/newmain.o.d build/default/production/newmain.o: \
+ newmain.c c:\avr8-gnu-toolchain-win32_x86\avr\include\stdio.h \
+ c:\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h \
+ c:\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h \
+ c:\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\avr8-gnu-toolchain-win32_x86\avr\include\stdlib.h
+
+c:\avr8-gnu-toolchain-win32_x86\avr\include\stdio.h:
+
+c:\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h:
+
+c:\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h:
+
+c:\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\avr8-gnu-toolchain-win32_x86\avr\include\stdlib.h:
