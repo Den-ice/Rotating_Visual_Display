93cdd9a1ef9daa90afe8596e934f76485302cba4
Sean8499 <50029716+Sean8499@users.noreply.github.com>
2019-12-08 23:42:08 -0800

    Create setup.h

---
+++ project/V3/setup.h
@@ -0,0 +1,23 @@
+
+#ifndef SETUP_H_
+#define SETUP_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+void system_clocks();
+//void rtc32_clock();
+void timer();
+void cap_comp();
+void gpio_setup();
+void setup_spi();
+void setup_timer();
+void setup_compare_timer();
+void tcd_pwm();
+void setup_dmac(uint32_t const src, uint32_t const dst);
+void tcd0_pwm_control(uint8_t time, uint16_t t_scale, float duty_cycle);
+unsigned long calc_period(uint8_t time, uint16_t t_scale);
+void tce_setup();
+
+#endif /* SETUP_H_ */
