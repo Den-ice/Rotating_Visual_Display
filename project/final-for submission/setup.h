fd60180f7cbd8c24ab4f24a624177a699568f711
Sean8499 <50029716+Sean8499@users.noreply.github.com>
2019-12-12 03:11:45 -0800

    Add files via upload

--- project/final-for submission/setup.h
+++ project/final-for submission/setup.h
@@ -12,22 +12,17 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void eventtrigger();
+
 
 void system_clocks();
 //void rtc32_clock();
-void timer();
-void cap_comp();
+void tcd_pwm();
+unsigned long calc_period(uint8_t time, uint16_t t_scale);
+void tcd0_pwm_control(uint8_t time, uint16_t t_scale, float duty_cycle);
 void gpio_setup();
 void setup_spi();
-void setup_timer();
-
-void setup_compare_timer();
-//motor pwm
-void tcd_pwm();
-void setup_dmac(uint32_t const src, uint32_t const dst);
-void tcd0_pwm_control(uint8_t time, uint16_t t_scale, float duty_cycle);
-unsigned long calc_period(uint8_t time, uint16_t t_scale);
+//void setup_dmac(uint32_t const src, uint32_t const dst);
 void tce_setup();
+void eventtrigger();
 
 #endif /* SETUP_H_ */
